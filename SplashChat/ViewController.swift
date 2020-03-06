d661f74c12710432e7eb9cc1b96d511bbacf69ff
Iranildo <iranildocarlos@localoeste.com.br>
2020-03-06 08:23:04 -0300

    Initial Commit

---
+++ SplashChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SplashChat
+//
+//  Created by Iranildo C. Silva on 06/03/20.
+//  Copyright © 2020 Local Oeste Software House. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
